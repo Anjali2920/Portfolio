7d82c4b712c6894661125b3dc391811a9a0b02a0
Anjali Santhakumar <anjali@nowce.com>
2024-07-11 12:20:10 +0100

    added download cv button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-
 import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
 
-export const Hero = () =>{
-    return  <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I'm Anjali</h1>
-            <p className={styles.description}>Im a full stack developer with 3 years of experience. Reach out if you'd like to learn more!</p>
-            <a href="mailto:anjalia1719@gmail.com" className={styles.contactBtn}>Contact me</a>
-        </div>
-        <img src={getImageUrl("hero/myImage.jpeg")} className={styles.heroImg}/>
-        <div className={styles.topBlur}/>
-        <div className={styles.bottomBlur}/>
-    </section>
-
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Anjali</h1>
+                <p className={styles.description}>I'm a full stack developer with 3 years of experience. Reach out if you'd like to learn more!</p>
+                <div className={styles.buttonContainer}>
+                    <a href="mailto:anjalia1719@gmail.com" className={styles.contactBtn}>Contact me</a>
+                    <a href="/cv.pdf" download className={styles.downloadBtn}>Download CV</a>
+                </div>
+            </div>
+            <img src={getImageUrl("hero/myImage.jpeg")} className={styles.heroImg} alt="Anjali's Image"/>
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+        </section>
+    );
 }
